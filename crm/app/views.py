657ce4a8a50bfe084a9da7337aab928f03df83da
Shreekant Kadam <10shreekant@gmail.com>
2024-12-23 22:27:52 +0530

    second commit

--- crm/app/views.py
+++ crm/app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+    
+def home(request):
+    # check to see if logging in:
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # Authenticate
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.warning(request, "There was an error logging in, Please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+    
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out !")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
